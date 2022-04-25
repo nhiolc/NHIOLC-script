64326de4902226bd604279301ddc392c4f61a1c2
nhiolc <104300665+nhiolc@users.noreply.github.com>
2022-04-25 15:32:53 +0200

    Add files via upload

---
+++ NHIOLC.py
@@ -0,0 +1,119 @@
+import pyfiglet
+from pyfiglet import figlet_format
+
+shell=True
+
+print(figlet_format("NHIOLC", font="big"))
+result = pyfiglet.figlet_format("Script created by NHIOLC", font="digital")
+print(result)
+
+print(" Hola,buenas, este es un programa creado por NHIOLC,"
+      " este programa esta dedicado para aprendizaje y la comprensión de python")
+print("¿Cual es su nombre?")
+nombre = input()
+print("Encantado de iniciar con usted " + nombre)
+print("Iniciaremos con operaciones simples y a medida que avances serán más complejas.")
+print("Tras terminar de leer una línea de texto presione 'enter' para avanzar.")
+
+input
+
+print("primera operación, crea un programa que resuelva una suma simple", input())
+print("La operación se resolverá de la siguiente forma:\n")
+print("print(20 + 45) \n")
+print("El resultado de la operación (Suma simple) es:")
+print(20 + 45)
+print("\npresione 'enter'")
+
+input
+
+print("Segunda operación, crea un programa que resuelva una resta simple.", input())
+print("La operación se resolverá de la siguiente forma:\n")
+print("print(20 - 10)\n")
+print("El resultado de la operación (Resta simple) es:")
+print(20 - 10)
+
+print("\npresione 'enter'")
+
+input
+
+print("Tercera operación, crea un programa que resuelva una división simple.", input())
+print("La operación se resolverá de la siguiente forma:\n")
+print("print(100 // 50)\n")
+print("El resultado de la operación (División simple) es:")
+print(100 // 50)
+
+print("\npresione 'enter'")
+
+input
+
+print("Cuarta operación, crea un programa que resuelva una multiplicación simple.", input())
+print("La operación se resolverá de la siguiente forma:\n")
+print("print(8 * 6)\n")
+print("El resultado de la operación (Multiplicación simple) es:")
+print(8 * 6)
+
+print("\npresione 'enter'")
+
+input
+
+print("Quinta operación, crea un programa que resuelva una operación mixta,"
+      " (multiplicaciones, divisiones, sumas y restas).", input())
+print("La operación se resolverá de la siguiente forma:\n")
+print("print(((10 + 15) * 10) // 2)\n")
+print("El resultado de la operación mixta es:")
+print(((10 + 15) * 10) // 2)
+
+print("\npresione 'enter'")
+
+input
+
+print("Sexta operación <Problemas>, Un avión viaja de España a Colombia, "
+      "la distancia entre estos es de 8033 km. ", input())
+print("Sabemos que la velocidad de el avión es de 689km/h. ¿Cuantas horas tardará en llegar a su destino? ")
+print("Crea un programa que resuelva este problema. ")
+print("La operación se resolverá de la siguiente forma:\n")
+print("print((8033 * 1) / 689, 'Horas')\n")
+print("El avión tardará: ")
+print((8033 * 1) / 689, "Horas")
+
+print("\npresione 'enter'")
+
+input
+
+print("Séptima operación <Variables>, crea un programa que resuelva una operación (cualquiera)"
+      " usando variables.", input())
+print("La operación se resolverá de la siguiente forma:\n")
+print("valor_uno = 1024")
+print("valor_dos = 2048")
+print("print(valor_uno + valor_dos)\n")
+print("El resultado de la operación con variables es:")
+valor_uno = 1024
+valor_dos = 2048
+print(valor_uno + valor_dos)
+
+print("\npresione 'enter'")
+
+input
+
+
+input
+
+print("Octava y última operación , crea un programa que resuelva una equación de 2º (x² -10x +8 = 0)", input())
+a = 1
+b = -10
+c = +8
+print("Primero sustituiremos valores y ubicaremos cada valor en su respectivo lugar de la formula =")
+print("Por favor, despues de cada línea, presione 'enter'")
+print(" [-(-10) +- √10² - 4 * 1 * 8 ] / 2 * 1 ", input())
+print("Ahora reducimos: ", input())
+print(" [10 +- √100 - 32] / 2 * 1 ", input())
+print("resolvemos: ", input())
+print(" [10 +- √68] / 2 * 1 = [[10 +- √8.24] / 2 * 1] ", input())
+print("Y por ultimo volvemos a resolver ", input())
+print("Primer resultado(positivo):", input())
+print((10 + 8.24) / 2)
+print("Segundo resultado (negativo):", input())
+print((10 - 8.24) / 2)
+input()
+print("Eso era todo\n")
+print("Gracias por su tiempo, nos volveremos a ver. " + nombre)
